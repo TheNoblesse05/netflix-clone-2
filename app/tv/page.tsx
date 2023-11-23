d583e722bc0fe38109aeb0be2732e507685f61a0
Vedant Tilwani <vedant-pawan.tilwani@hpe.com>
2023-11-24 02:48:44 +0530

    added multiple routing and video player

--- app/tv/page.tsx
+++ app/tv/page.tsx
@@ -1,8 +1,31 @@
+'use client'
+
+import CommonLayout from "@/components/common-layout/page";
+import { useEffect } from "react";
+
 export default function TV() {
-    return (
-      <>
-      <h1>TV</h1>
-      </>
-    )
-  }
-  
+
+  // Get mediaData according to different genre like Action, Romance and pass in to the CommonLayout component
+  // useEffect(() => {
+  //   async function getTV() {
+  //   }
+  //  }, [])
+
+  // Sample mediaData would look like:
+  // [
+  //   {
+  //       'title': 'Action',
+  //       'medias': [<Object>]
+  //   },
+  //   {
+  //       'title': 'Romance',
+  //       'medias': [<Object>]
+  //   }
+  // ]
+
+  return (
+    <main className="flex min-h-screen flex-col">
+      <CommonLayout mediaData={[]} />
+    </main>
+  )
+}
---
+++ app/watch/[...title]/page.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useEffect } from "react"
+import data from "@/movie_data/card_movies.json"
+import { useParams } from "next/navigation"
+import ReactPlayer from "react-player"
+
+export default function Watch() {
+
+    const params = useParams()
+    const paramsUrl = params.title[0].replace(/%20/g, " ")
+    console.log(paramsUrl)
+    let videoUrl = ''
+
+    // Ideally you'd want to pass the ID of the movie and search that ID in the DB to get the movie URL
+    // useEffect(() => {
+    //     async function getMovieURL(id)
+    // }, [])
+
+    data.forEach(movie => {
+        if (paramsUrl === movie.title)
+            videoUrl = movie.videoUrl || ''
+    })
+
+    console.log(videoUrl)
+
+    return (
+        <div>
+            <ReactPlayer
+                url={videoUrl}
+                width={'100%'}
+                height={'100%'}
+                style={{ position: 'absolute', top: '0', left: '0' }}
+                playing
+                controls
+            />
+        </div>
+    )
+}
